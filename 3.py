dbac30e91410152e3bbaa5e2ae6fca86756ee2ba
Zakhar Kokhan <zak-kokhan@yandex.ru>
2020-11-18 20:35:10 +0900

    22

--- 3.py
+++ 3.py
@@ -1,20 +1,9 @@
-# with open(r"files\file_3.txt", encoding="utf-8") as c_file:
-#     avg = []
-#     for rows in c_file.readlines():
-#         row = rows.split()
-#         try:
-#             print(f"{row[0]} получает меньше 20000.") if int(
-#                 row[1]) < 20000 else False
-#             avg.append(int(row[1])) if int(row[1]) > 0 else False
-#         except ValueError as e:
-#             pass
-# print(f"Средняя заработная плата: {sum(avg) / len(avg)}")
-
 try:
     with open(r"files\file_3.txt", encoding="utf-8") as r_file:
-        print(sum(list(int(array.split()[1])
+        print(sum(list(float(array.split()[1])
                        for array in r_file.readlines())))
+        r_file.seek(0)
         list(print(f'Доход меньше 20000 у {array.split()[0]}')
-             for array in r_file.readlines() if int(array.split()[1]) < 20000)
+             for array in r_file.readlines() if float(array.split()[1]) < 20000)
 except Exception as e:
     print(e)
