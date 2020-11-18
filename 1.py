54237473f0c8ea8361c6917375f800e31514b800
Zakhar Kokhan <zak-kokhan@yandex.ru>
2020-11-18 23:08:28 +0900

    исправлено 1 задание

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 try:
     with open(r"files\file_1.txt", "w", encoding="utf-8") as w_file:
         while True:
-            if list(w_file.write(input_var + '\n') for input_var in input("Введите данные: ").split()):
+            if list(w_file.writelines(input_var + '\n') for input_var in input("Введите данные: ").split()):
                 True
             else:
                 break
